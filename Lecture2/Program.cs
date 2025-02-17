695084d6c414fa3930cd1cc4f8d338512cc11127
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-17 18:05:02 +0200

    Lecture2: Classic object initialization

--- Lecture2/Program.cs
+++ Lecture2/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Initialization
+            Student mystudent1 = new Student();
+            mystudent1.FirstName = "Steve";
+            mystudent1.LastName = "Jobs";
+
+            Student mystudent2 = new Student();
+            mystudent1.FirstName = "Bill";
+            mystudent1.LastName = "Gates";
+            #endregion
         }
     }
 }
