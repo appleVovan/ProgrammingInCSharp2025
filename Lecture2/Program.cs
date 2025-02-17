36317bacb343890c00ce33a4f460cff583b71cfd
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-17 18:05:21 +0200

    Lecture2: simplified object initialization

--- Lecture2/Program.cs
+++ Lecture2/Program.cs
@@ -5,9 +5,11 @@
         static void Main(string[] args)
         {
             #region Initialization
-            Student mystudent1 = new Student();
-            mystudent1.FirstName = "Steve";
-            mystudent1.LastName = "Jobs";
+            Student mystudent1 = new Student
+            {
+                FirstName = "Steve",
+                LastName = "Jobs"
+            };
 
             Student mystudent2 = new Student();
             mystudent1.FirstName = "Bill";
