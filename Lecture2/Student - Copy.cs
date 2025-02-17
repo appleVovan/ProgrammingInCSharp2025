db627a0c18f56ccc2a0054c059112af4e0ce984a
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-17 18:17:56 +0200

    Lecture 2: Student class was copied

---
+++ Lecture2/Student - Copy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp2025.Lecture2
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
