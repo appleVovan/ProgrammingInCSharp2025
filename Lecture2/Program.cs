df5550877146f16b61b33e285a1479d3277ef5b2
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-17 17:21:51 +0200

    Lecture2: Project was added

---
+++ Lecture2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KMA.ProgrammingInCSharp2025.Lecture2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
