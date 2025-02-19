729f359342a09e896c4f52138c949264d339c46b
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-19 15:30:29 +0200

    Practice 2: SignInWindow was attached to app flow

--- Practice2LoginWindow/MainWindow.xaml.cs
+++ Practice2LoginWindow/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow
         public MainWindow()
         {
             InitializeComponent();
+            var signInWindow = new SignInWindow();
+            signInWindow.ShowDialog();
         }
     }
 }
