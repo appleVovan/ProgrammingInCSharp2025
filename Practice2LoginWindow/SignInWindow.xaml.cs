6f894525b1f9039c3b3f500422c7f8901923cf7b
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-19 15:58:34 +0200

    Practice 2: Buttons logic was implemented

--- Practice2LoginWindow/SignInWindow.xaml.cs
+++ Practice2LoginWindow/SignInWindow.xaml.cs
@@ -26,17 +26,29 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow
 
         private void BSignIn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (String.IsNullOrWhiteSpace(TbLogin.Text) || String.IsNullOrWhiteSpace(PbPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty");
+                return;
+            }
+            MessageBox.Show($"Login successful for user {TbLogin.Text}");
+            Close();
         }
 
         private void BSignUp_Click(object sender, RoutedEventArgs e)
         {
-
+            if (String.IsNullOrWhiteSpace(TbLogin.Text) || String.IsNullOrWhiteSpace(PbPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty");
+                return;
+            }
+            MessageBox.Show($"User with name {TbLogin.Text} was created!");
+            Close();
         }
 
         private void BCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            Environment.Exit(0);
         }
     }
 }
