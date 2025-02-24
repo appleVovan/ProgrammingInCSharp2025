36874336cbf9b2a091985016ece3ea0a680a126b
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-24 13:36:51 +0200

    Practice 3: TextBoxWtihCaption was added

---
+++ Practice2LoginWindow/Tools/Controls/TextBoxWithCaption.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Tools.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithCaption.xaml
+    /// </summary>
+    public partial class TextBoxWithCaption : UserControl
+    {
+        public TextBoxWithCaption()
+        {
+            InitializeComponent();
+        }
+    }
+}
