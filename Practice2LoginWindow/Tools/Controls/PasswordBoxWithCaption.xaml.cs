eef591a0e55d3f2c241bab98a86763e8502ee52f
applevovan <yablonskyi.v.reg@gmail.com>
2025-02-25 16:00:48 +0200

    Practice 3: PasswordBoxWithCaption was added

---
+++ Practice2LoginWindow/Tools/Controls/PasswordBoxWithCaption.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Tools.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithCaption.xaml
+    /// </summary>
+    public partial class PasswordBoxWithCaption : UserControl
+    {
+        public string Caption 
+        { 
+            get
+            {
+                return TbCaption.Text;
+            }
+            set
+            {
+                TbCaption.Text = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return PbValue.Password;
+            }
+            set
+            {
+                PbValue.Password = value;
+            }
+        }
+
+        public PasswordBoxWithCaption()
+        {
+            InitializeComponent();
+        }
+    }
+}
