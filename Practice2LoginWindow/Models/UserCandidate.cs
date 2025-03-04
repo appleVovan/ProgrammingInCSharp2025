59b8fdd6a1089bffd3161707f5162f698afe8a12
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-04 17:44:33 +0200

    Practice 4: Login and Password were added to UserCandidate

--- Practice2LoginWindow/Models/UserCandidate.cs
+++ Practice2LoginWindow/Models/UserCandidate.cs
@@ -8,5 +8,19 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models
 {
     class UserCandidate
     {
-    }
+		private string _login;
+        private string _password;
+
+        public string Login
+		{
+			get { return _login; }
+			set { _login = value; }
+		}
+		public string Password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
+
+	}
 }
