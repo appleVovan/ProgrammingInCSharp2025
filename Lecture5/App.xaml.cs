ff83fb4459ea21904d0d3e802b537d489b5fe4ca
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-11 17:50:22 +0200

    Lecture 5: Project was added

---
+++ Lecture5/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KMA.ProgrammingInCSharp2025.Lecture5
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
