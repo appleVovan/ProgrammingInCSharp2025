9fa8fc32283e1155c881169b7a5e1d937daa1872
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-12 18:53:08 +0200

    Practice 5: IMainNavigatable was added

---
+++ Practice2LoginWindow/Navigation/IMainNavigatable.cs
@@ -0,0 +1,14 @@
+﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Navigation
+{
+    interface IAuthNavigatable
+    {
+        public AuthNavigationType ViewModelType { get; }
+    }
+}
