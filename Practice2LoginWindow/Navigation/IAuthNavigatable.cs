a44e5c54d07492d54600bc4260b04f86cbaa346f
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-12 13:09:20 +0200

    Practice 5: IAuthNavigatable was added

---
+++ Practice2LoginWindow/Navigation/IAuthNavigatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Navigation
+{
+    interface IAuthNavigatable
+    {
+        public int ViewModelType { get; }
+    }
+}
