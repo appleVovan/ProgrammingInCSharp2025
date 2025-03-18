a53d7336fefba681f2fded3c32fe852237667090
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-18 21:06:31 +0200

    Lecture 6: Passing arguments with fields

--- Lecture6/Program.cs
+++ Lecture6/Program.cs
@@ -2,22 +2,29 @@
 {
     internal class Program
     {
+        private static Tuple<string, int, double> _inputPrams;
+        private static Tuple<string, int, double> _outputPrams;
         static void Main(string[] args)
         {
             Thread myParallelOperation = new Thread(MyTask);
 
-            myParallelOperation.Start(new object());
+            _inputPrams = new Tuple<string, int, double>("pram", 7, 1.1);
+
+            myParallelOperation.Start();
 
             //Perform some actions
 
             myParallelOperation.Join();
 
             //process results
+            Console.WriteLine(_outputPrams);
         }
 
-        static void MyTask(object obj)
+        static void MyTask()
         {
+            string param = _inputPrams.Item1;
             //Perform some operations
+            _outputPrams = new Tuple<string, int, double>("result", 3, 0.4);
         }
     }
 }
