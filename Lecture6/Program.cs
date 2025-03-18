9a0cc838e185b1a9e029fcb93c2741b827ef8ffd
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-18 20:39:54 +0200

    Lecture 6: Simple Thread example

--- Lecture6/Program.cs
+++ Lecture6/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Thread myParallelOperation = new Thread(MyTask);
+
+            myParallelOperation.Start();
+        }
+
+        static void MyTask()
+        {
+            //Perform some operations
         }
     }
 }
