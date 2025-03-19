e4a52937052ba2823289490476d18a528cb688c9
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-19 14:53:53 +0200

    Practice 6: User was implemented

--- Practice2LoginWindow/Models/User.cs
+++ Practice2LoginWindow/Models/User.cs
@@ -8,5 +8,17 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models
 {
     internal class User
     {
+        public Guid Guid { get; }
+        public string Login { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public User(Guid guid, string login, string firstName, string lastName)
+        {
+            Guid = guid;
+            Login = login;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
