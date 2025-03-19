e49638d14a4642d0a2dcf05573b994c8d46af50f
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-19 14:52:21 +0200

    Practice 6: DBUser was implemented

--- Practice2LoginWindow/Models/DBUser.cs
+++ Practice2LoginWindow/Models/DBUser.cs
@@ -8,5 +8,19 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models
 {
     internal class DBUser
     {
+        public Guid Guid { get; }
+        public string Login { get; }
+        public string Password { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public DBUser(string login, string password, string firstName, string lastName)
+        {
+            Guid = Guid.NewGuid();
+            Login = login;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
