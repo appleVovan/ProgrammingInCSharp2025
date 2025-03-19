13a64327c058d47a221820da9ebeda645f188cfc
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-19 15:00:15 +0200

    Practice 6: Authenticate method was added

--- Practice2LoginWindow/Services/AuthenticationService.cs
+++ Practice2LoginWindow/Services/AuthenticationService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,27 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Services
 {
     internal class AuthenticationService
     {
+        private static List<DBUser> Storage = new List<DBUser>();
+
+        public User Authenticate(UserCandidate userCandidate)
+        {
+            if (String.IsNullOrWhiteSpace(userCandidate.Login) || String.IsNullOrWhiteSpace(userCandidate.Password))
+                throw new ArgumentException("Login or Password is empty!");
+            DBUser dBUser = null;
+
+            foreach (DBUser user in Storage)
+            {
+                if (user.Login == userCandidate.Login && user.Password == userCandidate.Password)
+                {
+                    dBUser = user; 
+                    break;
+                }
+            }
+
+            if (dBUser == null)
+                throw new AuthenticationException("Wrong login or password!");
+
+            return new User(dBUser.Guid, dBUser.Login, dBUser.FirstName, dBUser.LastName);
+        }
     }
 }
