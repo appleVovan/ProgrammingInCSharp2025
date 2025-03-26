1bfc21f3b60e4eb741d972dcb9983200723e02ba
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-26 16:31:15 +0200

    Practice 7: Repository was used

--- Practice2LoginWindow/Services/AuthenticationService.cs
+++ Practice2LoginWindow/Services/AuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models;
+using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,35 +11,33 @@ namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Services
 {
     internal class AuthenticationService
     {
-        private static List<DBUser> Storage = new List<DBUser>();
+        private static FileRepository Storage = new FileRepository();
 
-        public User Authenticate(UserCandidate userCandidate)
+        public async Task<User> Authenticate(UserCandidate userCandidate)
         {
-            Thread.Sleep(3000);
             if (String.IsNullOrWhiteSpace(userCandidate.Login) || String.IsNullOrWhiteSpace(userCandidate.Password))
                 throw new ArgumentException("Login or Password is empty!");
-            DBUser dBUser = Storage.FirstOrDefault(user => user.Login == userCandidate.Login && user.Password == userCandidate.Password);
-            
-            if (dBUser == null)
+            DBUser dBUser = await Storage.GetAsync(userCandidate.Login);
+                        
+            if (dBUser == null || dBUser.Login != userCandidate.Login || dBUser.Password == userCandidate.Password)
                 throw new AuthenticationException("Wrong login or password!");
 
             return new User(dBUser.Guid, dBUser.Login, dBUser.FirstName, dBUser.LastName);
         }
 
-        public void RegisterUser(RegistrationUser registrationUser)
+        public async Task RegisterUser(RegistrationUser registrationUser)
         {
-            Thread.Sleep(3000);
             if (String.IsNullOrWhiteSpace(registrationUser.Login) || String.IsNullOrWhiteSpace(registrationUser.Password) || String.IsNullOrWhiteSpace(registrationUser.FirstName) || String.IsNullOrWhiteSpace(registrationUser.LastName))
                 throw new ArgumentException("First Name, Last Name, Login or Password is empty!");
 
-            DBUser dbUser = Storage.FirstOrDefault(user => user.Login == registrationUser.Login);
+            DBUser dbUser = await Storage.GetAsync(registrationUser.Login);
 
             if (dbUser != null)
                 throw new Exception("User with this login already exists!");
 
             dbUser = new DBUser(registrationUser.Login, registrationUser.Password, registrationUser.FirstName, registrationUser.LastName);
 
-            Storage.Add(dbUser);
+            await Storage.AddOrUpdateAsync(dbUser);
         }
     }
 }
