fbe648fce54bc30dc98895853418e34425bc3cb5
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-26 15:59:36 +0200

    Practice 7: ILoaderOwner was added

---
+++ Practice2LoginWindow/Managers/ILoaderOwner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Managers
+{
+    internal interface ILoaderOwner
+    {
+    }
+}
