a13c211cf6b09bf06bde3dd2240a38c2668b9983
applevovan <yablonskyi.v.reg@gmail.com>
2025-03-26 16:01:33 +0200

    Practice 7: ILoaderOwner was designed

--- Practice2LoginWindow/Managers/ILoaderOwner.cs
+++ Practice2LoginWindow/Managers/ILoaderOwner.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Managers
 {
     internal interface ILoaderOwner
     {
+        public bool IsEnabled { get; set; }
+
+        public Visibility LoaderVisibility { get; set; }
     }
 }
