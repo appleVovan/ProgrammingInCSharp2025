cefdabcb7df93ff821bf5ffa9403216add949436
applevovan <yablonskyi.v.reg@gmail.com>
2025-04-02 16:58:42 +0300

    Practice 8: UserService was added

---
+++ Practice2LoginWindow/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models;
+using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Services
+{
+    class UserService
+    {
+        private static FileRepository Repository = new FileRepository();
+
+        public List<User> GetAllUsers()
+        {
+            var res = new List<User>();
+
+            foreach (var dbUser in Repository.GetAll())
+            {
+                res.Add(new User(dbUser.Guid, dbUser.Login, dbUser.FirstName, dbUser.LastName));
+            }
+            return res;
+        }
+    }
+}
