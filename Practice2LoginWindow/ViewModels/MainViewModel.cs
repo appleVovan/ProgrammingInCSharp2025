a3fc725a3336f7cde8a30567392555b801f330a7
applevovan <yablonskyi.v.reg@gmail.com>
2025-04-02 17:00:20 +0300

    Practice 8: MainViewModel was implemented

--- Practice2LoginWindow/ViewModels/MainViewModel.cs
+++ Practice2LoginWindow/ViewModels/MainViewModel.cs
@@ -1,19 +1,53 @@
-﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Navigation;
+﻿using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Managers;
+using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Models;
+using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Navigation;
+using KMA.ProgrammingInCSharp2025.Practice2LoginWindow.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KMA.ProgrammingInCSharp2025.Practice2LoginWindow.ViewModels
 {
-    class MainViewModel : INavigatable<MainNavigationType>
+    class MainViewModel : INavigatable<MainNavigationType>, INotifyPropertyChanged
     {
         public MainNavigationType ViewModelType => MainNavigationType.Main;
+        private ObservableCollection<User> _users;
+
+        public ObservableCollection<User> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                OnProperyChanged();
+            }
+        }
+
+        public string CurrentUser 
+        { 
+            get
+            {
+                return $"Current User {ApplicationManager.CurrentUser.FirstName} {ApplicationManager.CurrentUser.LastName}";
+            }            
+        }
 
         public MainViewModel(Action logout)
         {
-            
+            _users = new ObservableCollection<User>(new UserService().GetAllUsers());
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnProperyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
